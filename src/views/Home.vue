2c962adf2cfa4af2ba4e9c4822c459fba86c5a6d
giddens9527 <jl4386@outlook.com>
2022-05-16 17:55:02 +0800

    change config for my own

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,15 @@
 <template>
-  <div>THIS IS HOME</div>
+  <div></div>
 </template>
 <script>
-import HomeApi from "@/api/modules/HomeApi";
 export default {
   name: "Home",
   components: {},
   computed: {},
   data: () => ({}),
-  beforeCreate() {},
   created() {},
-  beforeMount() {},
   mounted() {},
-  beforeUpdate() {},
-  updated() {},
-  activated() {}, //when keep-alive
-  deactivated() {}, //when keep-alive
-  beforeDestroy() {},
-  destroyed() {},
-  methods: {
-    // getDatList(){
-    //   HomeApi.getDatList()
-    // }
-  },
+  methods: {},
+  watch: {}
 };
 </script>
